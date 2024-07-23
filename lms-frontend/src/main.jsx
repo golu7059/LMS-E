e4a500cd7740f865f7349f6a46674aaced1bd315
Golu Kumar <golukumar@Golus-MacBook-Air.local>
2024-07-23 21:36:47 +0530

    Added react-hot-toast and eslint

--- lms-frontend/src/main.jsx
+++ lms-frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// components import
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// css import
+import "./index.css";
+
+// libraries import
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom"; // Better routing all over pages
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <BrowserRouter>
+      <App />
+      <Toaster />
+   </BrowserRouter>
+);
