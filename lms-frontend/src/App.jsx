55f644716b39ae1c4e7176531877c8ae85961c34
Golu Kumar <golukumar@Golus-MacBook-Air.local>
2024-07-26 22:57:35 +0530

    About us page complete

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import AboutUs from './Pages/AboutUs';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutUs />} />
       </Routes>
     </>
   );
